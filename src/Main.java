5efe093aa8b81234fbf335340bf0fe3a90daa009
Tuemin <ngaanh2.2510@gmail.com>
2023-08-24 21:39:10 +0700

    update

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+////        System.out.printf("Hello and welcome nga!");
+//        System.out.println("Yêu Hiệp");
+//        System.out.println("Yêu Hiệp");
+//        System.out.println("Yêu Hiệp");
+//
+//        // cach 3
+//        System.out.print("haha");
+// KDL bien = gtri
+        int number = 1;
+        byte numberByte = 1;
+        float numberFloat = 1.1f;  //fload de so nguyen khong loi, neus de so thap phan k looi thi phai la 1.1f
+        double numberDouble = 1.2;
+        boolean booleanResult = true ; // false (boolean is only true or false
+        System.out.println(number);
+        System.out.println(numberByte);
+        System.out.println(numberFloat);
+        System.out.println(numberDouble);
+        System.out.println(booleanResult);
+    }
+}
